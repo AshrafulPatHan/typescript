964658efe2888ae3e8e75860542637efe1bda46b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-22 16:32:41 +0600

    add function

--- 3_function.ts
+++ 3_function.ts
@@ -1,4 +1,18 @@
 // node 3_function.ts
 
+function chakNum(num:number) {
+    return num +2
+}
 
+chakNum(5)
+// many type
+function chakName(name:string , aeg:number , isPay:boolean) {
+    let pas = `The name is ${name} and aeg ${aeg}`
+}
+chakName("ha",30,true)
+// arrow function
+let arrow = () => {
+    
+}
 
+export{}

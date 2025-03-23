51aabebc252f2685413a9973cdc40229aa051177
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-23 19:38:11 +0600

    if condition

--- 4_if.ts
+++ 4_if.ts
@@ -1,3 +1,26 @@
+// node 4_if.ts
 
+function condition(value:number): boolean | string {
+    if (value > 5) {
+        return true
+    }
+    return "the number is bigger than 5"
+}
 
+let data = ["hi","by","tata"]
+data.map(data =>{
+    return `the data is ${data}`
+})
+
+let tata = ["hi",2,"tata"]
+tata.map((tata:string | number) =>{
+    return `the data is ${tata}`
+})
+
+let bay = ["hi",2,"tata"]
+tata.map((tata:string | number):string =>{
+    return `the data is ${bay}`
+})
+
+export{}
 

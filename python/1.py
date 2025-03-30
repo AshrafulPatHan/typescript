ff1d9b6bed833ddb5c21d3cac33c9192d618da11
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-30 10:19:52 +0600

    loop in pyton

--- python/1.py
+++ python/1.py
@@ -1,14 +1,19 @@
 # python3 1.py
 print("Hello, World!")
 
+# function
 def hello(): 
     print("hello")
 
+# if else
 def condition(a):
     if(a>5):
         print("the number is larger then 5")
-    print("the number is smaller then 5")
+    else:
+        print("the number is smaller then 5")
 
+
+# if elif
 def ifelse(a):
     if not isinstance(a, (int, float)):
         print("not a number")
@@ -21,4 +26,15 @@ def ifelse(a):
 ifelse(2)
 
 
+# loop 
 
+for x in range(6):
+    print("hello world")
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+for x in range(2):
+    print(x)

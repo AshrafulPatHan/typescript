12fefdcaeded4b4830e20e458dc7abe2da05d874
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 14:53:02 +0600

    masage

--- Project/src/index.ts
+++ Project/src/index.ts
@@ -14,16 +14,23 @@ class code {
         name:string,
         public readonly email:string,
         private aeg:number,
-        protected lang:string
+        protected lang:string = "typescript"
     ){
         this.name = name
         this.aeg = aeg
     }
+
+    public getAeg(){
+        return `my aeg is ${this.aeg} `
+    }
+
 }
 
+const Dave = new code("ashraful","ash@gamal",17,"typescript")
 
-
-
-
-
+console.log(Dave.getAeg);
+console.log(Dave.name);
+console.log(Dave.email);
+console.log(Dave.aeg);
+console.log(Dave.lang);
 

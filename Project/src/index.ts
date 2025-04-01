6e4d44fc30771b6b2db6f063068fb55766c3fa14
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 13:55:08 +0600

    class in src

--- Project/src/index.ts
+++ Project/src/index.ts
@@ -3,5 +3,27 @@
 
 console.log("the code is working still");
 
+// class
+class code {
+    name: string
+    // email:string
+    // aeg : number
+    time!: number
+
+    constructor(
+        name:string,
+        public readonly email:string,
+        private aeg:number,
+        protected lang:string
+    ){
+        this.name = name
+        this.aeg = aeg
+    }
+}
+
+
+
+
+
 
 

775d54e27bf0e2d233f9e8426c7c1cfba585c62e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 11:52:07 +0600

    class

---
+++ Typescript/12_class.ts
@@ -0,0 +1,18 @@
+
+class code {
+    name: string
+    aeg : number
+
+    constructor(
+        name:string,
+        aeg:number
+    ){
+        this.name = name
+        this.aeg = aeg
+    }
+}
+
+
+
+
+

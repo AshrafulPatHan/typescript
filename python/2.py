53d6aa2d77a5734ec28853bf6d9f52ec77fc618b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-03 10:12:31 +0600

    update

--- python/2.py
+++ python/2.py
@@ -27,6 +27,23 @@ poet = 2
 
 print(poet)
 
+number = 10
 
+if number <=5 :
+   print("howrah")
+elif number == 6 :
+   print(6)
+elif number == 7 :
+   print(7)
+elif number == 8 :
+   print(8)
+elif number == 9 :
+   print(9)
+elif number == 11 :
+   print(11)
+elif number <100 :
+   print("woo")
+elif number >100 :
+   print("100")
 
 

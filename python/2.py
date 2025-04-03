95608d10a39c14d70e9344b7fb2b47911ed693df
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-03 09:48:09 +0600

    add creat python logick

--- python/2.py
+++ python/2.py
@@ -4,13 +4,15 @@
 def chakAge(age):
    if age >= 18 :
       print("pay mony for food")
-   elif age > 70 :
+   elif age >= 70 :
       print("you have 10% discount")
-   elif age >= 12 :
+   elif age <= 17 :
       print("you have 50% discount")
    elif age <= 11 :
       print("you eat food for free")
-chakAge(13)
+chakAge(23)
+
+
 
 
 

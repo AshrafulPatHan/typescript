8233f0b30c832b8b282d32978d1984b84df1259b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-25 16:12:46 +0600

    add types

---
+++ Types/praktis.js
@@ -0,0 +1,24 @@
+// tsc praktis.js
+// declaring variable and type and console.log for print the code
+var start = "bismella";
+console.log(start);
+// declaring dynamic type 
+// ex -- 1
+var string = " ";
+string = "hello";
+console.log(string);
+var later = "";
+later = "mama";
+// ex -- 2
+var countMama;
+function gitNum() {
+    return 2;
+}
+;
+countMama = gitNum();
+// ex -- 3
+// const Bull:string|number ;
+// Bull = "kaku"
+var bullBull;
+bullBull = "kaku";
+// declaring function
--- Types/praktis.ts
+++ Types/praktis.ts
@@ -1,7 +1,51 @@
 // tsc praktis.js
 
+// declaring variable and type and console.log for print the code
 let start:string = "bismella";
 
 console.log(start);
+
+// declaring dynamic type 
+
+// ex -- 1
+let string : string = " "
+
+string = "hello"
+
+console.log(string);
+
+// ex -- 2
+
+type later = string;
+
+let later:later = ""
+
+later = "mama"
+
+// ex -- 2
+
+let countMama:number;
+
+function gitNum(){
+   return 2
+};
+countMama = gitNum()
+
+// ex -- 3
+
+// const Bull:string|number ;
+
+// Bull = "kaku"
+
+
+let bullBull:string|number;
+
+bullBull = "kaku"
+
+// declaring function
+
+
+
+
 
 

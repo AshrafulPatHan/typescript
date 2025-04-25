e61401376f9a4255a8122bc569e8cc164df21932
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-25 15:35:53 +0600

    after the long jerny

--- Typescript/10_enums.ts
+++ Typescript/10_enums.ts
@@ -7,4 +7,41 @@ enum minecraft {
 
 let FirstVersion = minecraft.firstVersion;
 
+// condition 2
 
+interface User{
+    name: string;
+    email: string
+}
+
+interface Admin {
+    name: string;
+    email: string;
+    isAdmin: boolean
+}
+
+function isAdminAccount(account:) {
+    if ("isAdmin" in account) {
+        return account.isAdmin
+    }
+}
+
+// condition 3 
+
+type Fish = {swim: ()=> void};
+type Bird = {fly: ()=> void};
+
+function isFish(pet:Fish|Bird):pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet:Fish|Bird) {
+    if(isFish(pet)){
+        pet
+        return "fish food"
+    }
+    else{
+        pet
+        return "bird food"
+    }
+}

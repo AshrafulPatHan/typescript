d1b5b1c55371073e3fec11880cb187ec75ca2b7d
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-25 16:58:14 +0600

    add mix array

--- Types/praktis.js
+++ Types/praktis.js
@@ -23,3 +23,71 @@ countMama = gitNum();
 // Bull = "kaku"
 var bullBull;
 bullBull = "kaku";
+// declaring function
+// 1
+function functions(type) {
+    return 2;
+}
+;
+functions("hello-2");
+// 2
+function functions_2(type, num, boll) {
+    return 2;
+}
+;
+functions_2("hello-2", 3, false);
+// 3
+function functions_3(type, num, boll) {
+    return "howrah";
+}
+;
+functions_3("hello-2", 3, false);
+var arrow = function (fun) {
+    return 2;
+};
+arrow("hi");
+// condition declaring
+function condition(value) {
+    if (value > 5) {
+        return true;
+    }
+    ;
+    return "the number is bigger than 5";
+}
+;
+condition(5);
+// data mapIng
+var tata = ["hi", 2, "tata"];
+tata.map(function (tata) {
+    return "the data is ".concat(tata);
+});
+var data = ["hello", "mama", false];
+data.map(function (data) {
+    return "tata is ".concat(data);
+});
+// Object declaring
+var Object = {
+    name: "ashraful",
+    aeg: 17,
+    employ: false
+};
+function myData(_a) {
+    var string = _a.name, number = _a.aeg, boolean = _a.employ;
+    return "Ashraful Pathan";
+}
+;
+// 2
+myData(Object);
+function MyData(_a) {
+    var string = _a.name, number = _a.aeg, boolean = _a.employ;
+    return { name: string, aeg: number, employ: boolean };
+}
+;
+MyData(Object);
+myData(Object);
+function MyAta(_a) {
+    var string = _a.name, number = _a.aeg, boolean = _a.employ;
+    return { name: string, aeg: number, employ: boolean };
+}
+;
+MyAta(Object);
--- Types/praktis.ts
+++ Types/praktis.ts
@@ -10,7 +10,7 @@ console.log(start);
 // ex -- 1
 let string : string = " "
 
-string = "hello"
+string = "hello";
 
 console.log(string);
 
@@ -18,18 +18,18 @@ console.log(string);
 
 type later = string;
 
-let later:later = ""
+let later:later = "";
 
-later = "mama"
+later = "mama";
 
 // ex -- 2
 
 let countMama:number;
 
 function gitNum(){
-   return 2
+   return 2;
 };
-countMama = gitNum()
+countMama = gitNum();
 
 // ex -- 3
 
@@ -40,35 +40,145 @@ countMama = gitNum()
 
 let bullBull:string|number;
 
-bullBull = "kaku"
+bullBull = "kaku";
 
 // declaring function
 
 // 1
 
 function functions(type:string) {
-   return 2
+   return 2;
 };
-functions("hello-2")
+functions("hello-2");
 
 // 2
 
 function functions_2(type:string,num:number,boll:boolean) {
-   return 2
+   return 2;
 };
-functions_2("hello-2",3,false)
+functions_2("hello-2",3,false);
 
 // 3
 
 function functions_3(type:string,num:number,boll:boolean):string {
-   return "howrah"
+   return "howrah";
 };
-functions_3("hello-2",3,false)
+functions_3("hello-2",3,false);
 
 let arrow = (fun:string):number => {
-   return 2
+   return 2;
+};
+arrow("hi");
+
+// condition declaring
+
+function condition(value:number): boolean | string {
+   if (value > 5) {
+       return true;
+   };
+   return "the number is bigger than 5";
+};
+condition(5);
+
+// data mapIng
+let tata = ["hi",2,"tata"];
+tata.map((tata:string | number) =>{
+    return `the data is ${tata}`;
+});
+
+const data = ["hello","mama",false];
+data.map((data:string|boolean) => {
+   return`tata is ${data}`;
+});
+
+// Object declaring
+
+let Object = {
+   name:"ashraful",
+   aeg:17,
+   employ : false
+};
+
+function myData({name:string,aeg:number,employ:boolean}) {
+   return "Ashraful Pathan";
+};
+
+// 2
+
+myData(Object)
+function MyData({name:string,aeg:number,employ:boolean}):{name:string,aeg:number,employ:boolean} {
+   return {name:string,aeg:number,employ:boolean};
+};
+myData(Object);
+
+// 3
+
+type myData = {name:string,aeg:number,employ:boolean};
+
+
+function MyAta({name:string,aeg:number,employ:boolean}):myData {
+   return {name:string,aeg:number,employ:boolean};
+};
+MyAta(Object);
+
+// 4
+
+// function MydatA(myData):myData {
+//    return {name:string,aeg:number,employ:boolean};
+// };
+// MydatA(Object);
+
+// Types |||||
+type User = {
+   readonly name: string;
+   aeg: number;
+   isPay: boolean
 }
-arrow("hi")
+
+function createUser(data:User) {
+   
+}
+
+// Array
+
+const Array = ["Ashraful",17,false]
+
+const inside : number[][] = [
+   [1,2,]
+]
+const arrayObject : { hi: string; rating: number }[] = [
+   {
+      hi:"hello",
+      rating : 10
+   }
+];
+
+const mixedArray: (number | { hi: string; rating: number })[] = [
+   5,
+   {
+   hi: "hello",
+   rating: 10
+   }
+];
+ 
+ 
+const mixedArray2:any = [
+   5,
+   {
+      hi: "hello",
+      rating: 10
+   }
+];
+  
+
+
+
+
+
+
+
+
+
 
 
 
